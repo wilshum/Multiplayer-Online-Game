1c58a5f61623f9fa62970014b981b12f6d665299
ProPillow <wilsonshum96@hotmail.com>
2018-02-17 20:50:08 -0800

    bug fix

--- cilent/scripts/script.js
+++ cilent/scripts/script.js
@@ -72,6 +72,15 @@ socket.on('addToChat', function (data) {
     chatText.innerHTML += '<div>' + data + '</div>';
 });
 
+function inTextField(event) {
+    var elem = event.target || event.srcElement;
+    if (elem.nodeType == 3)
+        elem = elem.parentNode;
+
+    return (elem.tagName == "TEXTAREA" ||
+    (elem.tagName == "INPUT" && (elem.getAttribute("type") == "text")));
+}
+
 chatForm.onsubmit = function (event) {
     event.preventDefault();
     if (chatInput.value.substring(0, 1) === "/")
@@ -148,25 +157,29 @@ socket.on('Time', function () {
 });
 
 document.onkeydown = function (event) {
-    if (event.keyCode === 68) //d
-        socket.emit('keyPress', {inputId: 'right', state: true});
-    else if (event.keyCode === 83)  //s
-        socket.emit('keyPress', {inputId: 'down', state: true});
-    else if (event.keyCode === 65) //a
-        socket.emit('keyPress', {inputId: 'left', state: true});
-    else if (event.keyCode === 87) //w
-        socket.emit('keyPress', {inputId: 'up', state: true});
+    if (!inTextField(event)) {
+        if (event.keyCode === 68) //d
+            socket.emit('keyPress', {inputId: 'right', state: true});
+        else if (event.keyCode === 83)  //s
+            socket.emit('keyPress', {inputId: 'down', state: true});
+        else if (event.keyCode === 65) //a
+            socket.emit('keyPress', {inputId: 'left', state: true});
+        else if (event.keyCode === 87) //w
+            socket.emit('keyPress', {inputId: 'up', state: true});
+    }
 };
 
 document.onkeyup = function (event) {
-    if (event.keyCode === 68) //d
-        socket.emit('keyPress', {inputId: 'right', state: false});
-    else if (event.keyCode === 83)  //s
-        socket.emit('keyPress', {inputId: 'down', state: false});
-    else if (event.keyCode === 65) //a
-        socket.emit('keyPress', {inputId: 'left', state: false});
-    else if (event.keyCode === 87) //w
-        socket.emit('keyPress', {inputId: 'up', state: false});
+    if (!inTextField(event)) {
+        if (event.keyCode === 68) //d
+            socket.emit('keyPress', {inputId: 'right', state: false});
+        else if (event.keyCode === 83)  //s
+            socket.emit('keyPress', {inputId: 'down', state: false});
+        else if (event.keyCode === 65) //a
+            socket.emit('keyPress', {inputId: 'left', state: false});
+        else if (event.keyCode === 87) //w
+            socket.emit('keyPress', {inputId: 'up', state: false});
+    }
 };
 
 function drawChar(player) {
